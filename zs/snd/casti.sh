445b2702e256be824b5a5a8d1905384c2b98a6b8
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-11 10:12:22 +0000

    🫦

---
+++ zs/snd/casti.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+# This bash srcript is for installing the KL docker image here
+clear
+
+# Colors
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export WHITE='\033[0;37m'
+export NC='\033[0m' # No Color
+
+# Commands
+
+hea1() {
+    echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+    echo -e "${PURPLE}$1${NC}"
+    echo -e "${CYAN}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${NC}"
+}
+
+b1() {
+    hea1 "UV Jupyter Lan Installation Commands"
+}
+
+c1() {
+    hea1 "Use cast to convert UTF8 to hex"
+    dataz="Smell Her Ass and Pussy"
+    cmd1="cast fa $dataz"
+
+    output=()
+}
+
+# Execution
+c1
