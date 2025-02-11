f57dfcee442b5ebbf0999c4b74c81fd56278df58
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-11 18:30:54 +0000

    🫦

--- zs/snd/s1/index.ts
+++ zs/snd/s1/index.ts
@@ -7,7 +7,8 @@ async function main() {
 ╔═╝ ╠╩╗ ╚═╗ ╚╦╝ ║║║ ║   Alchemy Send Tx ZkSync 
 ╚═╝ ╩ ╩ ╚═╝  ╩  ╝╚╝ ╚═╝
 `);
-    await drawBox2("Wallet Send Tx ZkSync");
+    await drawBox2(`W1: 0xb66A5CfDb06F3C40Ba9dC911BE686112E32cf2e6
+W2: 0x58855397faae1468F00534bFb5eD43f1430b6E9e`);
 }
 
 //execute function
