155e7f72df8bb2ae658f59df55b32b22a583c150
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-11 10:32:31 +0000

    🫦

--- zs/snd/casti.sh
+++ zs/snd/casti.sh
@@ -24,6 +24,7 @@ b1() {
     hea1 "UV Jupyter Lan Installation Commands"
 }
 
+# Function will ask for an input and convert to hex
 encode_to_hex_input() {
     hea1 "Use cast to convert UTF8 to hex"
     echo -e ""
@@ -50,7 +51,28 @@ encode_to_hex_input() {
     echo -e "${GREEN} ${hex_in} ${NC}"
     echo "---input---" >>${file_name}
     echo "$hex_in" >>${file_name}
+}
 
+#Function that will convert data stored in variable to hex
+encodedata__to_hex_input() {
+    hea1 "Use cast to convert UTF8 Data to hex"
+    dataz
+
+    cmd1="cast fa \"$dataz\""
+    hex_out=$(eval "$cmd1")
+    cmd2="cast tas $hex_out"
+    hex_in=$(eval "$cmd2")
+    file_name="out.txt"
+
+    echo -e "${CYAN}---Output---${NC}"
+    echo -e "${GREEN} ${hex_out} ${NC}"
+    echo "---Output---" >${file_name}
+    echo "$hex_out" >>${file_name}
+    echo -e ""
+    echo -e "${YELLOW}---Input---${NC}"
+    echo -e "${GREEN} ${hex_in} ${NC}"
+    echo "---input---" >>${file_name}
+    echo "$hex_in" >>${file_name}
 }
 
 # Execution
