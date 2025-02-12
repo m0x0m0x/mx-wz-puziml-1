6f0d248ddf42adcb7712634772d274910ee497d3
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-02-12 02:33:24 +0000

    🫦

--- WO/hfz/uvinit.sh
+++ WO/hfz/uvinit.sh
@@ -37,7 +37,7 @@ uvsetup() {
     CO1="uv init $name_of_project"
     CO2="cd $name_of_project"
 
-    DEPS="rich "
+    DEPS="rich requests"
     CO3="uv add  $DEPS"
     CO4="uv tree"
 
@@ -54,4 +54,4 @@ uvsetup() {
 }
 
 # Execution
-menu_choice
+uvsetup
